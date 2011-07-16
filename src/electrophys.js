c4d372bb1b596db8b3438666fde0dafc735bc802
Kendrick Shaw <kms15@case.edu>
2011-07-16 03:31:14 +0000

    added Getting et al IF neuron

--- src/electrophys.js
+++ src/electrophys.js
@@ -44,3 +44,67 @@ electrophys.pulse = function (options) {
             }
         };
 }
+
+
+electrophys.gettingIFNeuron = function (model, options) {
+    var C = options.C;
+    var g_leak = options.g_leak;
+    var E_leak = options.E_leak;
+    var theta_ss = options.theta_ss;
+    var theta_r = options.theta_r;
+    var theta_tau = options.theta_tau;
+    var currents = [];
+
+    var iV = model.addStateVar(E_leak);
+    var iTheta = model.addStateVar(theta_ss);
+    
+    function drift(result, state, t) {
+        var i = currents.length;
+        var I_inj = 0;
+        
+        while (i > 0) {
+            --i;
+            I_inj += currents[i](state, t);
+        }
+
+        result[iV] = (g_leak * (E_leak - state[iV]) + I_inj)/C;
+        result[iTheta] = (theta_ss - state[iTheta])/theta_tau;
+    }
+
+    function jump(state, t) {
+        if (state[iV] >= state[iTheta]) {
+            state[iTheta] = theta_r;
+            return true;
+        }
+    }
+
+    function VWithSpikes(state, t) {
+        if (state[iV] instanceof Array) {
+            var newV = state[iV].slice(0);
+            var theta = state[iTheta];
+            var i = theta.length - 1;
+            
+            while (i > 0) {
+                --i;
+                if (theta[i] < theta[i+1]) {
+                    newV[i+1] = 55e-3;
+                }
+            }
+
+            return newV;
+        }
+        else {
+            return state[iV];
+        }
+    }
+
+    model.registerDrift(drift);
+    model.registerJump(jump);
+    
+    return {
+        V : function (state, t) { return state[iV]; },
+        VWithSpikes : VWithSpikes,
+        theta : function (state, t) { return state[iTheta]; },
+        addCurrent : function (I) { currents.push(I); }
+    };
+}
--- test/electrophys_test.js
+++ test/electrophys_test.js
@@ -13,13 +13,13 @@ TestCase("PassiveMembrane", {
     },
 
     "test should set initial condition to be leak potential" : function () {
-        assertEquals(-65e-3, this.model.initialValues()[0]);
+        assertEquals(-65e-3, this.passiveMembrane.V(this.model.initialValues(), 0));
     },
 
     "test should have expected drift" : function () {
         var dy = Array(this.model.numStateVars());
         this.drift(dy, [-75e-3], 0.);
-        assertClose(0.25, this.passiveMembrane.V(dy));
+        assertClose(0.25, this.passiveMembrane.V(dy, 0));
     },
 
     "test should pass state to input currents" : function () {
@@ -83,3 +83,110 @@ TestCase("Pulse", {
         assertEquals(0.525, this.pulse([], 0.2999));
     }
 });
+
+
+TestCase("GettingIFNeuron", {
+    setUp: function () {
+        this.model = componentModel.componentModel();
+        sinon.spy(this.model, "registerDrift");
+        sinon.spy(this.model, "registerJump");
+        
+        this.gettingIFNeuron = electrophys.gettingIFNeuron(this.model, 
+            { C: 2e-9, g_leak: 50e-9, E_leak: -65e-3, 
+                theta_ss: -40e-3, theta_r: 5e-3, theta_tau: 10e-3 });
+        this.drift = this.model.registerDrift.args[0][0];
+        this.jump = this.model.registerJump.args[0][0];
+    },
+
+    "test should have two state vars" : function () {
+        assertEquals(2, this.model.numStateVars());
+    },
+
+    "test should set initial voltage to be leak potential" : function () {
+        assertEquals(-65e-3, this.gettingIFNeuron.V(this.model.initialValues(), 0));
+    },
+
+    "test should set initial theta to be steady state" : function () {
+        assertEquals(-40e-3, this.gettingIFNeuron.theta(this.model.initialValues(), 0));
+    },
+
+    "test should have expected drift" : function () {
+        var dy = Array(this.model.numStateVars());
+        this.drift(dy, [-75e-3, -20e-3 ], 0.);
+        assertClose(0.25, this.gettingIFNeuron.V(dy));
+        assertClose(-2, this.gettingIFNeuron.theta(dy));
+    },
+
+    "test should not jump when below theta" : function () {
+        var originalState = [-10e-3, -5e-3];
+        var state = originalState.slice(0);
+        
+        var jumped = this.jump(state, 0);
+
+        assertUndefined(jumped);
+        assertEquals(originalState, state);
+    },
+
+    "test theta should jump to theta_r when V is above theta_ss" : function () {
+        var originalState = [-10e-3, -15e-3];
+        var state = originalState.slice(0);
+        
+        var jumped = this.jump(state, 0);
+
+        assertTrue(jumped);
+        assertEquals(-10e-3, this.gettingIFNeuron.V(state, 0));
+        assertEquals(5e-3, this.gettingIFNeuron.theta(state, 0));
+    },
+
+    "test should pass state to input currents" : function () {
+        var dy = Array(this.model.numStateVars());
+        var state = this.model.initialValues;
+        var t = 3.2;
+        var ICalled = false;
+
+        function I(state1, t1) {
+            ICalled = true;
+            assertEquals(state, state);
+            assertEquals(t, t1);
+
+            return 0;
+        };
+
+        this.gettingIFNeuron.addCurrent(I);
+        this.drift(dy, this.model.initialValues(), t);
+        assertTrue(ICalled);
+    },
+    
+    "test should sum input currents" : function () {
+        var withoutCurrents = Array(this.model.numStateVars());
+        var withCurrents = Array(this.model.numStateVars());
+        var state = this.model.initialValues;
+        var t = 3.2;
+
+        this.drift(withoutCurrents, this.model.initialValues(), 0.);
+
+        this.gettingIFNeuron.addCurrent(function () { return 1.34e-9; });
+        this.gettingIFNeuron.addCurrent(function () { return 2.4e-9; });
+
+        this.drift(withCurrents, this.model.initialValues(), 0.);
+
+        assertClose(1.87, this.gettingIFNeuron.V(withCurrents, t) 
+                - this.gettingIFNeuron.V(withoutCurrents, t));
+    },
+
+    "test VWithSpikes should return the voltage when called with a single state" 
+        : function () {
+        var state = [0.1, 0.2];
+        assertEquals(0.1, this.gettingIFNeuron.VWithSpikes(state));
+    },
+
+    "test VWithSpikes shoult return a copy with spikes wherever theta increases"
+        : function () {
+        var y = [[1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3], [ 10, 11, 9, 8, 9, 7 ]];
+        var withSpikes = this.gettingIFNeuron.VWithSpikes(y);
+
+        assertEquals([1e-3, 55e-3, 3e-3, 4e-3, 55e-3, 6e-3], withSpikes);
+        assertEquals([1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3], this.gettingIFNeuron.V(y));
+    }
+});
+

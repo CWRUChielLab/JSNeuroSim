2a79eafaf44d470cb907a6684ccd9f805b12c72c
Kendrick Shaw <kms15@case.edu>
2011-08-06 14:38:12 +0000

    JSLint cleanup for integration tests

--- integration_tests/electrophystest.js
+++ integration_tests/electrophystest.js
@@ -1,178 +1,220 @@
-// set up the controls for the passive membrane simulation
-function init() {
-    passiveMembraneParams = { 
-        paramA:{ label: "param A" }, 
-        paramB:{ label: "param B" }, 
-        paramC:{ label: "param C" }, 
-        paramD:{ label: "param D" } 
+/*jslint browser: true */
+/*global ode: true, graph: true, simcontrols: true, electrophys: true, 
+   componentModel: true */
+
+// wait until the document has loaded
+window.addEventListener('load', function () {
+    'use strict';
+
+    var params, layout, panel, controls;
+
+    // set up the controls for the passive membrane simulation
+    params = { 
+        paramA: { label: "param A" }, 
+        paramB: { label: "param B" }, 
+        paramC: { label: "param C" }, 
+        paramD: { label: "param D" } 
     };
-    passiveMembraneLayout = [
+    layout = [
         ['A Heading', ['paramA', 'paramB']],
         ['A Second Heading', ['paramC', 'paramD']],
     ];
-    passiveMembranePanel = document.getElementById('PassiveMembraneControls');
-    passiveMembraneControls = simcontrols.controls(
-        passiveMembranePanel, passiveMembraneParams, passiveMembraneLayout);
-}
-window.onload=init;
+    panel = document.getElementById('PassiveMembraneControls');
+    controls = simcontrols.controls(panel, params, layout);
 
-// simulate and plot a passive membrane with a pulse
-function plotPassiveMembrane()
-{
-    // get the drawing surface
-    var canvas = document.getElementById('PassiveMembranePlot');
-    var context = canvas.getContext('2d');
+    // simulate and plot a passive membrane with a pulse
+    function plotPassiveMembrane() {
+        var canvas, context, model, passiveMembrane,
+            result, t, v, timeAxis, vAxis, vPlot;
+        
+        // get the drawing surface
+        canvas = document.getElementById('PassiveMembranePlot');
+        context = canvas.getContext('2d');
 
-    // clear the canvas
-    canvas.width = canvas.width;
+        // clear the canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // create the passive membrane
-    var model = componentModel.componentModel();
-    var passiveMembrane = electrophys.passiveMembrane(model, 
-        { C: 2e-9, g_leak: 1e-6, E_leak: -65e-3 });
+        // create the passive membrane
+        model = componentModel.componentModel();
+        passiveMembrane = electrophys.passiveMembrane(model, 
+            { C: 2e-9, g_leak: 1e-6, E_leak: -65e-3 });
 
-    passiveMembrane.addCurrent(electrophys.pulse(
-        {start: 15e-3, width: 10e-3, height: 10e-9}));
-    
-    // simulate it
-    var result = model.integrate({tMin: 0, tMax: 50e-3});
-    var t = result.t;
-    var v = passiveMembrane.V(result.y, result.t);
+        passiveMembrane.addCurrent(electrophys.pulse(
+            {start: 15e-3, width: 10e-3, height: 10e-9}
+        ));
+        
+        // simulate it
+        result = model.integrate({tMin: 0, tMax: 50e-3});
+        t = result.t;
+        v = passiveMembrane.V(result.y, result.t);
 
-    // set up axes for the plot
-    var timeAxis = graph.linearAxis(t[0], t[t.length-1], 0, 500);
-    var vAxis = graph.linearAxis(-70e-3, -50e-3, 200, 0);
-    var vPlot = graph.plotArea(timeAxis, vAxis);
-       
-    // plot the results
-    vPlot.addXYLine(t, v);
-    vPlot.draw(context);
-}
+        // set up axes for the plot
+        timeAxis = graph.linearAxis(t[0], t[t.length - 1], 0, 500);
+        vAxis = graph.linearAxis(-70e-3, -50e-3, 200, 0);
+        vPlot = graph.plotArea(timeAxis, vAxis);
+           
+        // plot the results
+        vPlot.addXYLine(t, v);
+        vPlot.draw(context);
+    }
 
-
-// simulate and plot the C2 neuron from Calin-Jageman et al 2007
-function plotC2()
-{
-    // get the drawing surface
-    var canvas = document.getElementById('C2Plot');
-    var context = canvas.getContext('2d');
-
-    // clear the canvas
-    canvas.width = canvas.width;
-
-    // create the passive membrane
-    var model = componentModel.componentModel();
-    var C2 = electrophys.gettingIFNeuron(model, 
-        { C: 2.27e-9, g_leak: 1/23.3e6, E_leak: -48e-3, 
-            theta_ss: -34e-3, theta_r: 0e-3, theta_tau: 65.0e-3 });
-    var C2Fast = electrophys.gettingSynapse(model, C2, C2, 
-        { W: 0.12000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:   30e-3 });
-    var C2Med = electrophys.gettingSynapse(model, C2, C2, 
-        { W: 0.02800e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close: 1200e-3 });
-    var C2Slow = electrophys.gettingSynapse(model, C2, C2, 
-        { W: 0.00300e-6, E_rev: -80e-3, tau_open: 4000e-3, tau_close: 4000e-3 });
-
-    C2.addCurrent(electrophys.pulse(
-        {start: 1, width: 5, height: 2e-9}));
-    
-    // simulate it
-    var result = model.integrate({tMin: 0, tMax: 7, tMaxStep: 2e-3});
-    var t = result.t;
-    var v = C2.VWithSpikes(result.y, result.t);
-
-    // set up axes for the plot
-    var timeAxis = graph.linearAxis(t[0], t[t.length-1], 0, 500);
-    var vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
-    var vPlot = graph.plotArea(timeAxis, vAxis);
-       
-    // plot the results
-    vPlot.addXYLine(t, v);
-    vPlot.draw(context);
-}
+    document.getElementById('PassiveMembraneButton').addEventListener('click', 
+            plotPassiveMembrane, false);
+}, false);
 
 
-// simulate and plot the DSI neuron from Calin-Jageman et al 2007
-function plotDSI()
-{
-    // get the drawing surface
-    var canvas = document.getElementById('DSIPlot');
-    var context = canvas.getContext('2d');
+window.addEventListener('load', function () {
+    'use strict';
 
-    // clear the canvas
-    canvas.width = canvas.width;
+    // simulate and plot the C2 neuron from Calin-Jageman et al 2007
+    function plotC2() {
+        var canvas, context, model, C2, C2Fast, C2Med, C2Slow,
+            result, t, v, timeAxis, vAxis, vPlot;
 
-    // create the passive membrane
-    var model = componentModel.componentModel();
-    var DSI = electrophys.gettingIFNeuron(model, 
-        { C: 1.5714765e-9, g_leak: 1/38.8e6, E_leak: -47.5e-3, 
-            theta_ss: -50e-3, theta_r: 200e-3, theta_tau: 15.0e-3 });
-    var DSIShunt = electrophys.gettingShuntConductance(model, DSI,
-        { G: 0.08e-6, E_rev: -47.5e-3, B_m: 29e-3, C_m: -1e-3, tau_m: 10e-3,
-                B_h: -100e-3, C_h: 1e-3, tau_h: 100000e-3 });
-        //{ G: 1e-6, E_rev: -70e-3, B_m: 30e-3, C_m: -9e-3, tau_m: 10e-3,
-        //        B_h: 54e-3, C_h: 4e-3, tau_h: 600e-3 });
-    var DSIFast = electrophys.gettingSynapse(model, DSI, DSI, 
-        { W: 0.30000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:   85e-3 });
-    var DSISlow = electrophys.gettingSynapse(model, DSI, DSI, 
-        { W: 0.01200e-6, E_rev: -80e-3, tau_open:  200e-3, tau_close: 2800e-3 });
+        // get the drawing surface
+        canvas = document.getElementById('C2Plot');
+        context = canvas.getContext('2d');
 
-    DSI.addCurrent(electrophys.pulse(
-        {start: 0.5, width: 5, height: 3e-9}));
-    
-    // simulate it
-    var result = model.integrate({tMin: -0.6, tMax: 7, tMaxStep: 2e-3});
-    var t = result.t;
-    var v = DSI.VWithSpikes(result.y, result.t);
+        // clear the canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // set up axes for the plot
-    var timeAxis = graph.linearAxis(0, t[t.length-1], 0, 500);
-    var vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
-    var vPlot = graph.plotArea(timeAxis, vAxis);
-       
-    // plot the results
-    vPlot.addXYLine(t, v);
-    vPlot.draw(context);
-}
+        // create the passive membrane
+        model = componentModel.componentModel();
+        C2 = electrophys.gettingIFNeuron(model, 
+            { C: 2.27e-9, g_leak: 1 / 23.3e6, E_leak: -48e-3, 
+                theta_ss: -34e-3, theta_r: 0e-3, theta_tau: 65.0e-3 });
+        C2Fast = electrophys.gettingSynapse(model, C2, C2, 
+            { W: 0.12000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:   30e-3 });
+        C2Med = electrophys.gettingSynapse(model, C2, C2, 
+            { W: 0.02800e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close: 1200e-3 });
+        C2Slow = electrophys.gettingSynapse(model, C2, C2, 
+            { W: 0.00300e-6, E_rev: -80e-3, tau_open: 4000e-3, tau_close: 4000e-3 });
+
+        C2.addCurrent(electrophys.pulse(
+            {start: 1, width: 5, height: 2e-9}
+        ));
+        
+        // simulate it
+        result = model.integrate({tMin: 0, tMax: 7, tMaxStep: 2e-3});
+        t = result.t;
+        v = C2.VWithSpikes(result.y, result.t);
+
+        // set up axes for the plot
+        timeAxis = graph.linearAxis(t[0], t[t.length - 1], 0, 500);
+        vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
+        vPlot = graph.plotArea(timeAxis, vAxis);
+           
+        // plot the results
+        vPlot.addXYLine(t, v);
+        vPlot.draw(context);
+    }
+
+    document.getElementById('C2Button').addEventListener('click', 
+            plotC2, false);
+}, false);
 
 
-// simulate and plot the VSI neuron from Calin-Jageman et al 2007
-function plotVSI()
-{
-    // get the drawing surface
-    var canvas = document.getElementById('VSIPlot');
-    var context = canvas.getContext('2d');
+window.addEventListener('load', function () {
+    'use strict';
 
-    // clear the canvas
-    canvas.width = canvas.width;
+    // simulate and plot the DSI neuron from Calin-Jageman et al 2007
+    function plotDSI() {
+        var canvas, context, model, DSI, DSIShunt, DSIFast, DSISlow,
+            result, t, v, timeAxis, vAxis, vPlot;
 
-    // create the passive membrane
-    var model = componentModel.componentModel();
-    var VSI = electrophys.gettingIFNeuron(model, 
-        { C: 3.2e-9, g_leak: 1/14e6, E_leak: -56e-3, 
-            theta_ss: -38e-3, theta_r: 10e-3, theta_tau: 10.0e-3 });
-    var VSIShunt = electrophys.gettingShuntConductance(model, VSI,
-        { G: 1e-6, E_rev: -70e-3, B_m: 30e-3, C_m: -9e-3, tau_m: 10e-3,
-                B_h: 54e-3, C_h: 4e-3, tau_h: 600e-3 });
-    var VSIFast = electrophys.gettingSynapse(model, VSI, VSI, 
-        { W: 0.54000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:  100e-3 });
-    var VSISlow = electrophys.gettingSynapse(model, VSI, VSI, 
-        { W: 0.00460e-6, E_rev: -80e-3, tau_open: 1000e-3, tau_close: 2500e-3 });
+        // get the drawing surface
+        canvas = document.getElementById('DSIPlot');
+        context = canvas.getContext('2d');
 
-    VSI.addCurrent(electrophys.pulse(
-        {start: 0.5, width: 5, height: 2.5e-9}));
+        // clear the canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // simulate it
-    var result = model.integrate({tMin: -0.6, tMax: 7, tMaxStep: 2e-3});
-    var t = result.t;
-    var v = VSI.VWithSpikes(result.y, result.t);
+        // create the passive membrane
+        model = componentModel.componentModel();
+        DSI = electrophys.gettingIFNeuron(model, 
+            { C: 1.5714765e-9, g_leak: 1 / 38.8e6, E_leak: -47.5e-3, 
+                theta_ss: -50e-3, theta_r: 200e-3, theta_tau: 15.0e-3 });
+        DSIShunt = electrophys.gettingShuntConductance(model, DSI,
+            { G: 0.08e-6, E_rev: -47.5e-3, B_m: 29e-3, C_m: -1e-3, tau_m: 10e-3,
+                    B_h: -100e-3, C_h: 1e-3, tau_h: 100000e-3 });
+            //{ G: 1e-6, E_rev: -70e-3, B_m: 30e-3, C_m: -9e-3, tau_m: 10e-3,
+            //        B_h: 54e-3, C_h: 4e-3, tau_h: 600e-3 });
+        DSIFast = electrophys.gettingSynapse(model, DSI, DSI, 
+            { W: 0.30000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:   85e-3 });
+        DSISlow = electrophys.gettingSynapse(model, DSI, DSI, 
+            { W: 0.01200e-6, E_rev: -80e-3, tau_open:  200e-3, tau_close: 2800e-3 });
 
-    // set up axes for the plot
-    var timeAxis = graph.linearAxis(0, t[t.length-1], 0, 500);
-    var vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
-    var vPlot = graph.plotArea(timeAxis, vAxis);
-       
-    // plot the results
-    vPlot.addXYLine(t, v);
-    vPlot.draw(context);
-}
+        DSI.addCurrent(electrophys.pulse(
+            {start: 0.5, width: 5, height: 3e-9}
+        ));
+        
+        // simulate it
+        result = model.integrate({tMin: -0.6, tMax: 7, tMaxStep: 2e-3});
+        t = result.t;
+        v = DSI.VWithSpikes(result.y, result.t);
 
+        // set up axes for the plot
+        timeAxis = graph.linearAxis(0, t[t.length - 1], 0, 500);
+        vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
+        vPlot = graph.plotArea(timeAxis, vAxis);
+           
+        // plot the results
+        vPlot.addXYLine(t, v);
+        vPlot.draw(context);
+    }
+
+    document.getElementById('DSIButton').addEventListener('click', 
+            plotDSI, false);
+}, false);
+
+
+window.addEventListener('load', function () {
+    'use strict';
+
+    // simulate and plot the VSI neuron from Calin-Jageman et al 2007
+    function plotVSI() {
+        var canvas, context, model, VSI, VSIShunt, VSIFast, VSISlow,
+            result, t, v, timeAxis, vAxis, vPlot;
+
+        // get the drawing surface
+        canvas = document.getElementById('VSIPlot');
+        context = canvas.getContext('2d');
+
+        // clear the canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // create the passive membrane
+        model = componentModel.componentModel();
+        VSI = electrophys.gettingIFNeuron(model, 
+            { C: 3.2e-9, g_leak: 1 / 14e6, E_leak: -56e-3, 
+                theta_ss: -38e-3, theta_r: 10e-3, theta_tau: 10.0e-3 });
+        VSIShunt = electrophys.gettingShuntConductance(model, VSI,
+            { G: 1e-6, E_rev: -70e-3, B_m: 30e-3, C_m: -9e-3, tau_m: 10e-3,
+                    B_h: 54e-3, C_h: 4e-3, tau_h: 600e-3 });
+        VSIFast = electrophys.gettingSynapse(model, VSI, VSI, 
+            { W: 0.54000e-6, E_rev: -80e-3, tau_open:   10e-3, tau_close:  100e-3 });
+        VSISlow = electrophys.gettingSynapse(model, VSI, VSI, 
+            { W: 0.00460e-6, E_rev: -80e-3, tau_open: 1000e-3, tau_close: 2500e-3 });
+
+        VSI.addCurrent(electrophys.pulse(
+            {start: 0.5, width: 5, height: 2.5e-9}
+        ));
+
+        // simulate it
+        result = model.integrate({tMin: -0.6, tMax: 7, tMaxStep: 2e-3});
+        t = result.t;
+        v = VSI.VWithSpikes(result.y, result.t);
+
+        // set up axes for the plot
+        timeAxis = graph.linearAxis(0, t[t.length - 1], 0, 500);
+        vAxis = graph.linearAxis(-80e-3, 55e-3, 200, 0);
+        vPlot = graph.plotArea(timeAxis, vAxis);
+           
+        // plot the results
+        vPlot.addXYLine(t, v);
+        vPlot.draw(context);
+    }
+
+    document.getElementById('VSIButton').addEventListener('click', 
+            plotVSI, false);
+}, false);
--- integration_tests/odetest.js
+++ integration_tests/odetest.js
@@ -1,45 +1,58 @@
-// simulate and plot a simple harmonic oscillator
-function plotSHO()
-{
-    // get the drawing surface
-    var canvas = document.getElementById('SHOPlot');
-    var context = canvas.getContext('2d');
+/*jslint browser: true */
+/*global ode: true, graph:true */
 
-    // clear the canvas
-    canvas.width = canvas.width;
+// wait until the document has loaded
+window.addEventListener('load', function () {
+    'use strict';
+    
+    // simulate and plot a simple harmonic oscillator
+    function plotSHO() {
 
-    // simulate the oscillator
-    sho = {
-        tMin: 0,
-        tMax: 100,
-        tMaxStep: 0.1,
-        drift: function (y, t) { return [y[1], -y[0]]; }, 
-        y0: [1, 0]
-    };
-    var result = ode.integrate(sho)
-    t = result.t;
-    x = result.y[0];
-    v = result.y[1];
+        // get the drawing surface
+        var canvas, context,
+            sho, result, t, x, v, 
+            phaseXAxis, phaseVAxis, phasePlot,
+            timeAxis, xAxis, vAxis, xPlot, vPlot;
 
-    // set up axes for the phase plane plot
-    var phaseXAxis = graph.linearAxis(-1.5, 1.5, 0, 400);
-    var phaseVAxis = graph.linearAxis(-1.5, 1.5, 0, 400);
-    var phasePlot = graph.plotArea(phaseXAxis, phaseVAxis);
+        // set up the canvas
+        canvas = document.getElementById('SHOPlot');
+        context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-    var timeAxis = graph.linearAxis(sho.tMin, sho.tMax, 0, 400);
-    var xAxis = graph.linearAxis(-1.5, 1.5, 400, 500);
-    var vAxis = graph.linearAxis(-1.5, 1.5, 500, 600);
-    var xPlot = graph.plotArea(timeAxis, xAxis);
-    var vPlot = graph.plotArea(timeAxis, vAxis);
-       
-    // plot the results
-    phasePlot.addXYLine(x, v);
-    xPlot.addXYLine(t, x);
-    vPlot.addXYLine(t, v);
+        // simulate the oscillator
+        sho = {
+            tMin: 0,
+            tMax: 100,
+            tMaxStep: 0.1,
+            drift: function (y, t) { return [y[1], -y[0]]; }, 
+            y0: [1, 0]
+        };
+        result = ode.integrate(sho);
+        t = result.t;
+        x = result.y[0];
+        v = result.y[1];
 
-    phasePlot.draw(context);
-    xPlot.draw(context);
-    vPlot.draw(context);
-}
+        // set up axes for the phase plane plot
+        phaseXAxis = graph.linearAxis(-1.5, 1.5, 0, 400);
+        phaseVAxis = graph.linearAxis(-1.5, 1.5, 0, 400);
+        phasePlot = graph.plotArea(phaseXAxis, phaseVAxis);
 
+        timeAxis = graph.linearAxis(sho.tMin, sho.tMax, 0, 400);
+        xAxis = graph.linearAxis(-1.5, 1.5, 400, 500);
+        vAxis = graph.linearAxis(-1.5, 1.5, 500, 600);
+        xPlot = graph.plotArea(timeAxis, xAxis);
+        vPlot = graph.plotArea(timeAxis, vAxis);
+           
+        // plot the results
+        phasePlot.addXYLine(x, v);
+        xPlot.addXYLine(t, x);
+        vPlot.addXYLine(t, v);
 
+        phasePlot.draw(context);
+        xPlot.draw(context);
+        vPlot.draw(context);
+    }
+
+    document.getElementById('SHOButton').addEventListener('click', 
+            plotSHO, false);
+}, false);

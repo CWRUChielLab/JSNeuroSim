1ea16def9dd2130b7639ff697ffddce001f747eb
Kendrick Shaw <kms15@case.edu>
2011-10-19 15:08:43 +0000

    NO synapse checkpoint

--- simulations/synapse_currentclamp_no.js
+++ simulations/synapse_currentclamp_no.js
@@ -6,15 +6,22 @@
 window.addEventListener('load', function () {
     'use strict';
 
-    var params, layout, controlsPanel, controls, tMax = 1000e-3; 
+    var params, layout, controlsPanel, controls, tMax = 10000e-3; 
 
     // set up the controls for the current clamp simulation
     params = { 
+        prestim_freq_Hz: { label: 'Prestimulation frequency', units: 'Hz',
+            defaultVal: 50, minVal: 0.001, maxVal: 200 },
+        prestim_duration_s: { label: 'Prestimulation duration', units: 's',
+            defaultVal: 0, minVal: 0, maxVal: 1000 },
+        prestim_delay_min: { 
+            label: 'Delay between prestimulation and experiment', units: 'min',
+            defaultVal: 10, minVal: 0, maxVal: 1000 },
         SNP_uM: { label: 'Sodium Nitroprusside', units: '\u00B5M',
             defaultVal: 0, minVal: 0, maxVal: 2000 },
 
         C_pre_nF: { label: 'Membrane capacitance', units: 'nF',
-            defaultVal: 0.2, minVal: 0.001, maxVal: 100 }, 
+            defaultVal: 0.1, minVal: 0.001, maxVal: 100 }, 
         g_leak_pre_uS: { label: 'Leak conductance', units: '\u00B5S', 
             defaultVal: 0.007, minVal: 0.0001, maxVal: 100 }, 
         E_leak_pre_mV: { label: 'Leak potential', units: 'mV',
@@ -34,12 +41,12 @@ window.addEventListener('load', function () {
         pulseWidth_pre_ms: { label: 'Pulse duration', units: 'ms', 
             defaultVal: 4, minVal: 0, maxVal: tMax / 1e-3 },
         isi_pre_ms: { label: 'Inter-stimulus interval', units: 'ms', 
-            defaultVal: 50, minVal: 0, maxVal: tMax / 1e-3 },
+            defaultVal: 30, minVal: 0, maxVal: tMax / 1e-3 },
         numPulses_pre: { label: 'Number of pulses', units: '', 
             defaultVal: 2, minVal: 0, maxVal: 100 },
 
         g_syn_uS: { label: 'Maximum conductance', units: '\u00B5S', 
-            defaultVal: 0.1, minVal: 0, maxVal: 1000 },
+            defaultVal: 0.02, minVal: 0, maxVal: 1000 },
         E_rev_syn_mV: { label: 'Reversal potential', units: 'mV', 
             defaultVal: 0, minVal: -1000, maxVal: 1000 },
         tau_r_ms: { label: 'Rise time constant', units: 'ms', 
@@ -52,7 +59,7 @@ window.addEventListener('load', function () {
             defaultVal: 10, minVal: 0.1, maxVal: 1000 },
 
         C_post_nF: { label: 'Membrane capacitance', units: 'nF',
-            defaultVal: 0.2, minVal: 0.001, maxVal: 100 }, 
+            defaultVal: 0.1, minVal: 0.001, maxVal: 100 }, 
         g_leak_post_uS: { label: 'Leak conductance', units: '\u00B5S', 
             defaultVal: 0.007, minVal: 0.001, maxVal: 100 }, 
         E_leak_post_mV: { label: 'Leak potential', units: 'mV',
@@ -77,10 +84,11 @@ window.addEventListener('load', function () {
             defaultVal: 2, minVal: 0, maxVal: 100 },
 
         totalDuration_ms: { label: 'Total duration', units: 'ms', 
-            defaultVal: 100, minVal: 0, maxVal: tMax / 1e-3 }
+            defaultVal: 60, minVal: 0, maxVal: tMax / 1e-3 }
     };
     layout = [
-        ['Pretreatment', ['SNP_uM']],
+        ['Pretreatment', ['prestim_freq_Hz', 'prestim_duration_s',
+            'prestim_delay_min', 'SNP_uM']],        
         ['Presynaptic Current Clamp', ['pulseStart_pre_ms', 
             'pulseHeight_pre_nA', 'pulseWidth_pre_ms', 'isi_pre_ms', 
             'numPulses_pre']],
@@ -108,13 +116,45 @@ window.addEventListener('load', function () {
             v_pre, v_pre_mV, iStim_pre_nA, 
             v_post, v_post_mV, iStim_post_nA,
             params, plotPanel, title, j,
-            f_NO, f_Na, f_K; 
+            sum1, sum2, dt, t_delay, k, NO_syn,
+            f_NO, f_Na, f_K,
+            delay_NO = 60, tau_NO = 600, NO_syn_scale = 1 / (0.35 * 60 * 50); 
         
         params = controls.values;
         model = componentModel.componentModel();
 
-        f_NO = 1/(1 + 10/(1e-6 + params.SNP_uM));
+        // sum up the effects of the presynaptic stimulation as a series of
+        // alpha functions.  Algorithm for summation from
+        // Srinivasan R, Chiel HJ. Fast Calculation of Synaptic Conductances.
+        // Neural Computation 1993 Mar;5(2):200-204.
+        sum1 = 0;
+        sum2 = 0;
+        dt = 1/params.prestim_freq_Hz;
+        t_delay = params.prestim_delay_min*60 - delay_NO;
+        k = Math.exp(-dt/tau_NO);
+        // process each of the spikes
+        for (t = -params.prestim_duration_s - Math.min(0, t_delay); 
+                t < 0; t += dt) {
+            sum1 = k*sum1 + 1;
+            sum2 = k*sum2 + t - dt;
+        }
+        console.log(sum1 + ', ' + sum2 + ', k=' + k);
+        // advance time to the beginning of the experiment
+        sum1 = Math.exp(-t_delay / tau_NO)*sum1;
+        sum2 = Math.exp(-t_delay / tau_NO)*sum2;
+        NO_syn = NO_syn_scale/tau_NO * (t_delay * sum1 - sum2);
+        NO_syn
+        console.log(NO_syn);
+
+        // Based on model and data from
+        // Steinert JR, Kopp-Scheinpflug C, Baker C, Challiss RAJ, Mistry R,
+        // Haustein MD, Griffin SJ, Tong H, Graham BP, Forsythe ID. Nitric
+        // oxide is a volume transmitter regulating postsynaptic excitability
+        // at a glutamatergic synapse. Neuron 2008 Nov;60(4):642-656.
+
+        f_NO = 1/(1 + 1/(1e-6 + params.SNP_uM/10 + 10*NO_syn*NO_syn));
         f_Na = (800 - 350 * f_NO) / 800;
+        //f_Na = 1;
         f_K = (140 - 92 * f_NO) / 140;
 
         // create the presynaptic passive membrane
@@ -228,7 +268,7 @@ window.addEventListener('load', function () {
             {xUnits: 'ms', yUnits: 'mV', minYRange: 20});
 
         title = document.createElement('h4');
-        title.innerHTML = 'Presynaptic Stimulation current (mV)';
+        title.innerHTML = 'Presynaptic Stimulation current (nA)';
         title.className = 'simplotheading';
         plotPanel.appendChild(title);
         graph.graph(plotPanel, 425, 70, t_ms, iStim_pre_nA,
@@ -242,7 +282,7 @@ window.addEventListener('load', function () {
             {xUnits: 'ms', yUnits: 'mV', minYRange: 20});
 
         title = document.createElement('h4');
-        title.innerHTML = 'Postsynaptic Stimulation current (mV)';
+        title.innerHTML = 'Postsynaptic Stimulation current (nA)';
         title.className = 'simplotheading';
         plotPanel.appendChild(title);
         graph.graph(plotPanel, 425, 70, t_ms, iStim_post_nA,

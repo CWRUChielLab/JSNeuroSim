b7df91907b8ff2fd97d42ded9ac35e1593ae84d6
Jeffrey Gill <jpg18@case.edu>
2012-09-01 14:04:09 -0400

    Updated plotting for Nernst passive membrane sim

--- simulations/nernstpassivemembrane_jqplot.js
+++ simulations/nernstpassivemembrane_jqplot.js
@@ -12,10 +12,18 @@ window.addEventListener('load', function () {
     params = { 
         C_nF: { label: 'Membrane capacitance', units: 'nF',
             defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
-        g_leak_uS: { label: 'Membrane conductance', units: '\u00B5S', 
-            defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
-        E_leak_mV: { label: 'Resting potential', units: 'mV',
-            defaultVal: 0, minVal: -1000, maxVal: 1000 }, 
+        g_Na_uS: { label: 'Membrane sodium conductance', units: '\u00B5S', 
+            defaultVal: 0.1, minVal: 0.01, maxVal: 100 }, 
+        E_Na_mV: { label: 'Sodium Nernst potential', units: 'mV',
+            defaultVal: 55, minVal: -1000, maxVal: 1000 }, 
+        g_K_uS: { label: 'Membrane potassium conductance', units: '\u00B5S', 
+            defaultVal: 3, minVal: 0.01, maxVal: 100 }, 
+        E_K_mV: { label: 'Potassium Nernst potential', units: 'mV',
+            defaultVal: -80, minVal: -1000, maxVal: 1000 }, 
+        g_Cl_uS: { label: 'Membrane chloride conductance', units: '\u00B5S', 
+            defaultVal: 0.1, minVal: 0.01, maxVal: 100 }, 
+        E_Cl_mV: { label: 'Chloride Nernst potential', units: 'mV',
+            defaultVal: -70, minVal: -1000, maxVal: 1000 }, 
         pulseStart_ms: { label: 'Stimulus delay', units: 'ms', 
             defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
         pulseHeight_nA: { label: 'Stimulus current', units: 'nA', 
@@ -30,28 +38,47 @@ window.addEventListener('load', function () {
             defaultVal: 40, minVal: 0, maxVal: tMax / 1e-3 }
     };
     layout = [
-        ['Cell Properties', ['C_nF', 'g_leak_uS', 'E_leak_mV']],
+        ['Cell Properties', ['C_nF', 'g_Na_uS', 'E_Na_mV', 'g_K_uS', 'E_K_mV',
+            'g_Cl_uS', 'E_Cl_mV']],
         ['Current Clamp', ['pulseStart_ms', 'pulseHeight_nA', 
             'pulseWidth_ms', 'isi_ms', 'numPulses']],
         ['Simulation Settings', ['totalDuration_ms']]
     ];
-    controlsPanel = document.getElementById('PassiveMembraneControls');
+    controlsPanel = document.getElementById('NernstPassiveMembraneControls');
 
     // simulate and plot a passive membrane with a pulse
     function runSimulation() {
-        var model, passiveMembrane, pulseTrain,
-            result, v, iLeak, iStim,
-            v_mV, iLeak_nA, params, iStim_nA,
+        var model, nernstPassiveMembrane, pulseTrain, V_steadyState, NaCurrent, KCurrent, ClCurrent,
+            result, v, iNa, iK, iCl, iStim,
+            v_mV, iNa_nA, iK_nA, iCl_nA, params, iStim_nA,
             plotPanel, plot, plotDefaultOptions;
         
-        // create the passive membrane
         params = controls.values;
+
+        V_steadyState = (params.g_Na_uS * params.E_Na_mV + 
+            params.g_K_uS * params.E_K_mV + params.g_Cl_uS * params.E_Cl_mV) /
+            (params.g_Na_uS + params.g_K_uS + params.g_Cl_uS) * 1e-3;
+        
+        // create the passive membrane
         model = componentModel.componentModel();
-        passiveMembrane = electrophys.passiveMembrane(model, {
+        nernstPassiveMembrane = electrophys.passiveMembrane(model, {
             C: params.C_nF * 1e-9, 
-            g_leak: params.g_leak_uS * 1e-6, 
-            E_leak: params.E_leak_mV * 1e-3 
+            g_leak: 0, 
+            E_leak: V_steadyState
         });
+
+        NaCurrent = electrophys.passiveConductance(
+            nernstPassiveMembrane, 
+            { g: params.g_Na_uS * 1e-6, E_rev: params.E_Na_mV * 1e-3 }
+        );
+        KCurrent = electrophys.passiveConductance(
+            nernstPassiveMembrane, 
+            { g: params.g_K_uS * 1e-6, E_rev: params.E_K_mV * 1e-3 }
+        );
+        ClCurrent = electrophys.passiveConductance(
+            nernstPassiveMembrane, 
+            { g: params.g_Cl_uS * 1e-6, E_rev: params.E_Cl_mV * 1e-3 }
+        );
 
         pulseTrain = electrophys.pulseTrain({
             start: 1e-3 * params.pulseStart_ms, 
@@ -60,28 +87,30 @@ window.addEventListener('load', function () {
             gap: params.isi_ms * 1e-3,
             num_pulses: params.numPulses
         });
-        passiveMembrane.addCurrent(pulseTrain);
+        nernstPassiveMembrane.addCurrent(pulseTrain);
         
-
         // simulate it
         result = model.integrate({
             tMin: 0, 
             tMax: params.totalDuration_ms * 1e-3, 
-            tMaxStep: Math.min(1e-4, params.C_nF / params.g_leak_uS * 1e-3) 
+            tMaxStep: Math.min(1e-4, params.C_nF / 
+                (params.g_Na_uS + params.g_K_uS + params.g_Cl_uS) * 1e-3) 
         });
         
-        v     = result.mapOrderedPairs(passiveMembrane.V);
-        iLeak = result.mapOrderedPairs(passiveMembrane.leak.current);
+        v     = result.mapOrderedPairs(nernstPassiveMembrane.V);
+        iNa   = result.mapOrderedPairs(NaCurrent.current);
+        iK    = result.mapOrderedPairs(KCurrent.current);
+        iCl   = result.mapOrderedPairs(ClCurrent.current);
         iStim = result.mapOrderedPairs(pulseTrain);
 
-        // convert to the right units
-        // each ordered pair consists of a time and another variable
         v_mV     = v.map     (function (v) {return [v[0] / 1e-3,  v[1] / 1e-3];});
-        iLeak_nA = iLeak.map (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iNa_nA   = iNa.map   (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iK_nA    = iK.map    (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iCl_nA   = iCl.map   (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
         iStim_nA = iStim.map (function (i) {return [i[0] / 1e-3,  i[1] / 1e-9];});
 
         // plot the results
-        plotPanel = document.getElementById('PassiveMembranePlots');
+        plotPanel = document.getElementById('NernstPassiveMembranePlots');
         plotPanel.innerHTML = '';
         plotDefaultOptions = {
             grid: {
@@ -117,13 +146,13 @@ window.addEventListener('load', function () {
             }
         };
 
-       // Voltage
-       plot = document.createElement('div');
-       plot.id = 'voltagePlot';
-       plot.style.width = '425px';
-       plot.style.height = '200px';
-       plotPanel.appendChild(plot);
-       $.jqplot('voltagePlot', [v_mV], jQuery.extend(true, {}, plotDefaultOptions, {
+        // Voltage
+        plot = document.createElement('div');
+        plot.id = 'voltagePlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        $.jqplot('voltagePlot', [v_mV], jQuery.extend(true, {}, plotDefaultOptions, {
             cursor: {
                 tooltipFormatString: "%s: %.2f ms, %.2f mV",
             },
@@ -142,7 +171,8 @@ window.addEventListener('load', function () {
         plot.style.width = '425px';
         plot.style.height = '200px';
         plotPanel.appendChild(plot);
-        $.jqplot('currentPlot', [iLeak_nA], jQuery.extend(true, {}, plotDefaultOptions, {
+        $.jqplot('currentPlot', [iNa_nA, iK_nA, iCl_nA], jQuery.extend(true, {}, plotDefaultOptions, {
+            legend: {show: true},
             cursor: {
                 tooltipFormatString: "%s: %.2f ms, %.2f nA",
             },
@@ -151,7 +181,9 @@ window.addEventListener('load', function () {
                 yaxis: {label:'Membrane Current (nA)'},
             },
             series: [
-                {label: 'I<sub>leak</sub>', color: 'black'},
+                {label: 'I<sub>Na</sub>',   color: 'blue'},
+                {label: 'I<sub>K</sub>',    color: 'red'},
+                {label: 'I<sub>Cl</sub>',   color: 'green'},
             ],
         }));
 
@@ -174,7 +206,6 @@ window.addEventListener('load', function () {
             ],
         }));
     }
-
     
     function reset() {
         controlsPanel.innerHTML = '';
@@ -182,14 +213,13 @@ window.addEventListener('load', function () {
         runSimulation();
     }
 
-
-    (document.getElementById('PassiveMembraneRunButton')
+    (document.getElementById('NernstPassiveMembraneRunButton')
         .addEventListener('click', runSimulation, false));
-    (document.getElementById('PassiveMembraneResetButton')
+    (document.getElementById('NernstPassiveMembraneResetButton')
         .addEventListener('click', reset, false));
     
-
-    // make the enter key run the simulation  
+    // make the enter key run the simulation (after a slight delay to allow
+    // the edit box to fire a change event first).  
     controlsPanel.addEventListener('keydown',  
         function (evt, element) {
             if (evt.keyCode === 13) { // enter was pressed 
@@ -202,4 +232,3 @@ window.addEventListener('load', function () {
     reset();
 
 }, false);
-

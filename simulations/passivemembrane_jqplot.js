59a62f2d509b058b6e1eca1583aaf56ff8fac01b
Jeffrey Gill <jpg18@case.edu>
2012-09-01 13:21:23 -0400

    Updated plotting for passive membrane sim

---
+++ simulations/passivemembrane_jqplot.js
@@ -0,0 +1,205 @@
+/*jslint browser: true */
+/*global ode: true, graph: true, simcontrols: true, electrophys: true, 
+   componentModel: true */
+
+// wait until the document has loaded
+window.addEventListener('load', function () {
+    'use strict';
+
+    var params, layout, controlsPanel, controls, tMax = 1000e-3; 
+
+    // set up the controls for the passive membrane simulation
+    params = { 
+        C_nF: { label: 'Membrane capacitance', units: 'nF',
+            defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
+        g_leak_uS: { label: 'Membrane conductance', units: '\u00B5S', 
+            defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
+        E_leak_mV: { label: 'Resting potential', units: 'mV',
+            defaultVal: 0, minVal: -1000, maxVal: 1000 }, 
+        pulseStart_ms: { label: 'Stimulus delay', units: 'ms', 
+            defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
+        pulseHeight_nA: { label: 'Stimulus current', units: 'nA', 
+            defaultVal: 10, minVal: -1000, maxVal: 1000 },
+        pulseWidth_ms: { label: 'Pulse duration', units: 'ms', 
+            defaultVal: 4, minVal: 0, maxVal: tMax / 1e-3 },
+        isi_ms: { label: 'Inter-stimulus interval', units: 'ms', 
+            defaultVal: 1, minVal: 0, maxVal: tMax / 1e-3 },
+        numPulses: { label: 'Number of pulses', units: '', 
+            defaultVal: 2, minVal: 0, maxVal: 100 },
+        totalDuration_ms: { label: 'Total duration', units: 'ms', 
+            defaultVal: 40, minVal: 0, maxVal: tMax / 1e-3 }
+    };
+    layout = [
+        ['Cell Properties', ['C_nF', 'g_leak_uS', 'E_leak_mV']],
+        ['Current Clamp', ['pulseStart_ms', 'pulseHeight_nA', 
+            'pulseWidth_ms', 'isi_ms', 'numPulses']],
+        ['Simulation Settings', ['totalDuration_ms']]
+    ];
+    controlsPanel = document.getElementById('PassiveMembraneControls');
+
+    // simulate and plot a passive membrane with a pulse
+    function runSimulation() {
+        var model, passiveMembrane, pulseTrain,
+            result, v, iLeak, iStim,
+            v_mV, iLeak_nA, params, iStim_nA,
+            plotPanel, plot, plotDefaultOptions;
+        
+        // create the passive membrane
+        params = controls.values;
+        model = componentModel.componentModel();
+        passiveMembrane = electrophys.passiveMembrane(model, {
+            C: params.C_nF * 1e-9, 
+            g_leak: params.g_leak_uS * 1e-6, 
+            E_leak: params.E_leak_mV * 1e-3 
+        });
+
+        pulseTrain = electrophys.pulseTrain({
+            start: 1e-3 * params.pulseStart_ms, 
+            width: params.pulseWidth_ms * 1e-3, 
+            height: params.pulseHeight_nA * 1e-9,
+            gap: params.isi_ms * 1e-3,
+            num_pulses: params.numPulses
+        });
+        passiveMembrane.addCurrent(pulseTrain);
+        
+
+        // simulate it
+        result = model.integrate({
+            tMin: 0, 
+            tMax: params.totalDuration_ms * 1e-3, 
+            tMaxStep: Math.min(1e-4, params.C_nF / params.g_leak_uS * 1e-3) 
+        });
+        
+        v     = result.mapOrderedPairs(passiveMembrane.V);
+        iLeak = result.mapOrderedPairs(passiveMembrane.leak.current);
+        iStim = result.mapOrderedPairs(pulseTrain);
+
+        // convert to the right units
+        // each ordered pair consists of a time and another variable
+        v_mV     = v.map     (function (v) {return [v[0] / 1e-3,  v[1] / 1e-3];});
+        iLeak_nA = iLeak.map (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iStim_nA = iStim.map (function (i) {return [i[0] / 1e-3,  i[1] / 1e-9];});
+
+        // plot the results
+        plotPanel = document.getElementById('PassiveMembranePlots');
+        plotPanel.innerHTML = '';
+        plotDefaultOptions = {
+            grid: {
+                shadow: false,
+            },
+            legend: {
+                placement: 'outside',
+            },
+            cursor: {
+                show: true,
+                zoom: true,
+                looseZoom: false,
+                followMouse: true,
+                useAxesFormatters: false,
+                showVerticalLine: true,
+                showTooltipDataPosition: true,
+                tooltipFormatString: "%s: %.2f, %.2f",
+            },
+            axes: {
+                xaxis: {
+                    min: 0,
+                    max: params.totalDuration_ms,
+                    tickOptions: {formatString: '%.2f'},
+                },
+            },
+            axesDefaults: {
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+            },
+            seriesDefaults: {
+                showMarker: false,
+                lineWidth: 1,
+                shadow: false,
+            }
+        };
+
+       // Voltage
+       plot = document.createElement('div');
+       plot.id = 'voltagePlot';
+       plot.style.width = '425px';
+       plot.style.height = '200px';
+       plotPanel.appendChild(plot);
+       $.jqplot('voltagePlot', [v_mV], jQuery.extend(true, {}, plotDefaultOptions, {
+            cursor: {
+                tooltipFormatString: "%s: %.2f ms, %.2f mV",
+            },
+            axes: {
+                xaxis: {label:'Time (ms)'},
+                yaxis: {label:'Membrane Potential (mV)'},
+            },
+            series: [
+                {label: 'V<sub>m</sub>', color: 'black'},
+            ],
+        }));
+
+        // Currents
+        plot = document.createElement('div');
+        plot.id = 'currentPlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        $.jqplot('currentPlot', [iLeak_nA], jQuery.extend(true, {}, plotDefaultOptions, {
+            cursor: {
+                tooltipFormatString: "%s: %.2f ms, %.2f nA",
+            },
+            axes: {
+                xaxis: {label:'Time (ms)'},
+                yaxis: {label:'Leak Current (nA)'},
+            },
+            series: [
+                {label: 'I<sub>leak</sub>', color: 'black'},
+            ],
+        }));
+
+        // Stimulus current
+        plot = document.createElement('div');
+        plot.id = 'stimPlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        $.jqplot('stimPlot', [iStim_nA], jQuery.extend(true, {}, plotDefaultOptions, {
+            cursor: {
+                tooltipFormatString: "%s: %.2f ms, %.2f nA",
+            },
+            axes: {
+                xaxis: {label:'Time (ms)'},
+                yaxis: {label:'Stimulation Current (nA)'},
+            },
+            series: [
+                {label: 'I<sub>stim</sub>', color: 'black'},
+            ],
+        }));
+    }
+
+    
+    function reset() {
+        controlsPanel.innerHTML = '';
+        controls = simcontrols.controls(controlsPanel, params, layout);
+        runSimulation();
+    }
+
+
+    (document.getElementById('PassiveMembraneRunButton')
+        .addEventListener('click', runSimulation, false));
+    (document.getElementById('PassiveMembraneResetButton')
+        .addEventListener('click', reset, false));
+    
+
+    // make the enter key run the simulation  
+    controlsPanel.addEventListener('keydown',  
+        function (evt, element) {
+            if (evt.keyCode === 13) { // enter was pressed 
+                controls.triggerRead();
+                runSimulation();
+                return false;
+            }
+        }, true);
+
+    reset();
+
+}, false);
+

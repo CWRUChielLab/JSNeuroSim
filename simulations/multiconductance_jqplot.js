f277dfb582653ef724b93d94040fca5f80b08c1c
Jeffrey Gill <jpg18@case.edu>
2013-06-17 11:34:04 -0400

    Added HH K current to multiconductance

--- simulations/multiconductance_jqplot.js
+++ simulations/multiconductance_jqplot.js
@@ -7,16 +7,21 @@ window.addEventListener('load', function () {
     'use strict';
 
     var params, layout, controlsPanel, controls, dataPanel, voltageDataTable,
-        currentDataTable, stimDataTable, tMax = 1000e-3, plotHandles = []; 
+        currentDataTable, conductanceDataTable, gateDataTable, stimDataTable,
+        tMax = 1000e-3, plotHandles = []; 
 
     // set up the controls for the passive membrane simulation
     params = { 
         C_nF: { label: 'Membrane capacitance', units: 'nF',
             defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
-        g_leak_uS: { label: 'Membrane conductance', units: '\u00B5S', 
+        g_leak_uS: { label: 'Leak conductance', units: '\u00B5S', 
             defaultVal: 0.005, minVal: 0.001, maxVal: 100 }, 
-        E_leak_mV: { label: 'Resting potential', units: 'mV',
+        E_leak_mV: { label: 'Leak potential', units: 'mV',
             defaultVal: -50, minVal: -1000, maxVal: 1000 }, 
+        E_K_mV: { label: 'Potassium Nernst potential', units: 'mV',
+            defaultVal: -80, minVal: -1000, maxVal: 1000},
+        g_hhK_uS: { label: 'Delayed rectifier potassium conductance', units: '\u00B5S',
+            defaultVal: 1.3, minVal: 0.001, maxVal: 100},
         pulseStart_ms: { label: 'Stimulus delay', units: 'ms', 
             defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
         pulseHeight_nA: { label: 'Stimulus current', units: 'nA', 
@@ -32,6 +37,7 @@ window.addEventListener('load', function () {
     };
     layout = [
         ['Cell Properties', ['C_nF', 'g_leak_uS', 'E_leak_mV']],
+        ['Potassium Currents', ['E_K_mV', 'g_hhK_uS']],
         ['Current Clamp', ['pulseStart_ms', 'pulseHeight_nA', 
             'pulseWidth_ms', 'isi_ms', 'numPulses']],
         ['Simulation Settings', ['totalDuration_ms']]
@@ -50,21 +56,32 @@ window.addEventListener('load', function () {
     currentDataTable.className = 'datatable';
     dataPanel.appendChild(currentDataTable);
 
+    conductanceDataTable = document.createElement('table');
+    conductanceDataTable.className = 'datatable';
+    dataPanel.appendChild(conductanceDataTable);
+
+    gateDataTable = document.createElement('table');
+    gateDataTable.className = 'datatable';
+    dataPanel.appendChild(gateDataTable);
+
     stimDataTable = document.createElement('table');
     stimDataTable.className = 'datatable';
     dataPanel.appendChild(stimDataTable);
 
     // simulate and plot a passive membrane with a pulse
     function runSimulation() {
-        var model, passiveMembrane, pulseTrain,
+        var model, neuron, pulseTrain,
+            hhKCurrent,
             result, v, iLeak, iStim,
+            iHHK, gHHK,
             v_mV, iLeak_nA, params, iStim_nA,
+            iHHK_nA, gHHK_uS, nGate,
             plotPanel, plot;
         
         // create the passive membrane
         params = controls.values;
         model = componentModel.componentModel();
-        passiveMembrane = electrophys.passiveMembrane(model, {
+        neuron = electrophys.passiveMembrane(model, {
             C: params.C_nF * 1e-9, 
             g_leak: params.g_leak_uS * 1e-6, 
             E_leak: params.E_leak_mV * 1e-3 
@@ -77,7 +94,12 @@ window.addEventListener('load', function () {
             gap: params.isi_ms * 1e-3,
             num_pulses: params.numPulses
         });
-        passiveMembrane.addCurrent(pulseTrain);
+        neuron.addCurrent(pulseTrain);
+        
+        hhKCurrent = electrophys.multiConductance_hhKConductance(model, neuron, {
+            g_K: params.g_hhK_uS * 1e-6,
+            E_K: params.E_K_mV * 1e-3
+        });
         
 
         // simulate it
@@ -87,14 +109,20 @@ window.addEventListener('load', function () {
             tMaxStep: Math.min(1e-4, params.C_nF / params.g_leak_uS * 1e-3) 
         });
         
-        v     = result.mapOrderedPairs(passiveMembrane.V);
-        iLeak = result.mapOrderedPairs(passiveMembrane.leak.current);
+        v     = result.mapOrderedPairs(neuron.V);
+        iLeak = result.mapOrderedPairs(neuron.leak.current);
+        iHHK  = result.mapOrderedPairs(hhKCurrent.current);
+        gHHK  = result.mapOrderedPairs(hhKCurrent.g);
+        nGate = result.mapOrderedPairs(hhKCurrent.n);
         iStim = result.mapOrderedPairs(pulseTrain);
 
         // convert to the right units
         // each ordered pair consists of a time and another variable
         v_mV     = v.map     (function (v) {return [v[0] / 1e-3,  v[1] / 1e-3];});
         iLeak_nA = iLeak.map (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iHHK_nA  = iHHK.map  (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        gHHK_uS  = gHHK.map  (function (g) {return [g[0] / 1e-3,  g[1] / 1e-6];});
+        nGate    = nGate.map (function (n) {return [n[0] / 1e-3,  n[1]       ];});
         iStim_nA = iStim.map (function (i) {return [i[0] / 1e-3,  i[1] / 1e-9];});
 
         // free resources from old plots
@@ -132,17 +160,63 @@ window.addEventListener('load', function () {
         plot.style.height = '200px';
         plotPanel.appendChild(plot);
         plotHandles.push(
-            $.jqplot('currentPlot', [iLeak_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+            $.jqplot('currentPlot', [iLeak_nA, iHHK_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                legend: {show: true},
                 axes: {
                     xaxis: {label:'Time (ms)'},
-                    yaxis: {label:'Membrane Current (nA)'},
+                    yaxis: {label:'Current (nA)'},
                 },
                 series: [
                     {label: 'I<sub>leak</sub>', color: 'black'},
+                    {label: 'I<sub>K</sub>',  color: 'red'},
                 ],
         })));
-        graphJqplot.bindDataCapture('#currentPlot', currentDataTable, 'Membrane Current', 'Time');
+        graphJqplot.bindDataCapture('#currentPlot', currentDataTable, 'Current', 'Time');
         graphJqplot.bindCursorTooltip('#currentPlot', 'Time', 'ms', 'nA');
+
+        // Conductances
+        plot = document.createElement('div');
+        plot.id = 'conductancePlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+            $.jqplot('conductancePlot', [gHHK_uS], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                legend: {show: true},
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {label:'Conductance (\u00B5S)'},
+                },
+                series: [
+                    {label: 'g<sub>K</sub>',  color: 'red'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#conductancePlot', conductanceDataTable, 'Conductance', 'Time');
+        graphJqplot.bindCursorTooltip('#conductancePlot', 'Time', 'ms', '\u00B5S');
+
+        // Gates
+        plot = document.createElement('div');
+        plot.id = 'gatePlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+            $.jqplot('gatePlot', [nGate], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                legend: {show: true},
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {
+                        label:'Gate',
+                        min: 0, max: 1,
+                        numberTicks: 6,
+                    }
+                },
+                series: [
+                    {label: 'n', color: 'red'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#gatePlot', gateDataTable, 'Gate', 'Time');
+        graphJqplot.bindCursorTooltip('#gatePlot', 'Time', 'ms', '');
 
         // Stimulus current
         plot = document.createElement('div');
@@ -178,6 +252,12 @@ window.addEventListener('load', function () {
 
         currentDataTable.innerHTML = '';
         currentDataTable.style.display = 'none';
+
+        conductanceDataTable.innerHTML = '';
+        conductanceDataTable.style.display = 'none';
+
+        gateDataTable.innerHTML = '';
+        gateDataTable.style.display = 'none';
 
         stimDataTable.innerHTML = '';
         stimDataTable.style.display = 'none';

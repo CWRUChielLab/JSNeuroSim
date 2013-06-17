b436bee875c2b904d36f9e3c855cf683740598fb
Jeffrey Gill <jpg18@case.edu>
2013-06-17 10:01:33 -0400

    First pass at Butera model

---
+++ simulations/multiconductance_jqplot.js
@@ -0,0 +1,209 @@
+/*jslint browser: true */
+/*global ode: true, graph: true, simcontrols: true, electrophys: true, 
+   componentModel: true */
+
+// wait until the document has loaded
+window.addEventListener('load', function () {
+    'use strict';
+
+    var params, layout, controlsPanel, controls, dataPanel, voltageDataTable,
+        currentDataTable, stimDataTable, tMax = 1000e-3, plotHandles = []; 
+
+    // set up the controls for the passive membrane simulation
+    params = { 
+        C_nF: { label: 'Membrane capacitance', units: 'nF',
+            defaultVal: 2, minVal: 0.01, maxVal: 100 }, 
+        g_leak_uS: { label: 'Membrane conductance', units: '\u00B5S', 
+            defaultVal: 0.005, minVal: 0.001, maxVal: 100 }, 
+        E_leak_mV: { label: 'Resting potential', units: 'mV',
+            defaultVal: -50, minVal: -1000, maxVal: 1000 }, 
+        pulseStart_ms: { label: 'Stimulus delay', units: 'ms', 
+            defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
+        pulseHeight_nA: { label: 'Stimulus current', units: 'nA', 
+            defaultVal: 10, minVal: -1000, maxVal: 1000 },
+        pulseWidth_ms: { label: 'Pulse duration', units: 'ms', 
+            defaultVal: 4, minVal: 0, maxVal: tMax / 1e-3 },
+        isi_ms: { label: 'Inter-stimulus interval', units: 'ms', 
+            defaultVal: 1, minVal: 0, maxVal: tMax / 1e-3 },
+        numPulses: { label: 'Number of pulses', units: '', 
+            defaultVal: 2, minVal: 0, maxVal: 100 },
+        totalDuration_ms: { label: 'Total duration', units: 'ms', 
+            defaultVal: 40, minVal: 0, maxVal: tMax / 1e-3 }
+    };
+    layout = [
+        ['Cell Properties', ['C_nF', 'g_leak_uS', 'E_leak_mV']],
+        ['Current Clamp', ['pulseStart_ms', 'pulseHeight_nA', 
+            'pulseWidth_ms', 'isi_ms', 'numPulses']],
+        ['Simulation Settings', ['totalDuration_ms']]
+    ];
+    controlsPanel = document.getElementById('MultiConductanceControls');
+
+    // prepare tables for displaying captured data points
+    dataPanel = document.getElementById('MultiConductanceData');
+    dataPanel.className = 'datapanel';
+
+    voltageDataTable = document.createElement('table');
+    voltageDataTable.className = 'datatable';
+    dataPanel.appendChild(voltageDataTable);
+
+    currentDataTable = document.createElement('table');
+    currentDataTable.className = 'datatable';
+    dataPanel.appendChild(currentDataTable);
+
+    stimDataTable = document.createElement('table');
+    stimDataTable.className = 'datatable';
+    dataPanel.appendChild(stimDataTable);
+
+    // simulate and plot a passive membrane with a pulse
+    function runSimulation() {
+        var model, passiveMembrane, pulseTrain,
+            result, v, iLeak, iStim,
+            v_mV, iLeak_nA, params, iStim_nA,
+            plotPanel, plot;
+        
+        // create the passive membrane
+        params = controls.values;
+        model = componentModel.componentModel();
+        passiveMembrane = electrophys.passiveMembrane(model, {
+            C: params.C_nF * 1e-9, 
+            g_leak: params.g_leak_uS * 1e-6, 
+            E_leak: params.E_leak_mV * 1e-3 
+        });
+
+        pulseTrain = electrophys.pulseTrain({
+            start: 1e-3 * params.pulseStart_ms, 
+            width: params.pulseWidth_ms * 1e-3, 
+            height: params.pulseHeight_nA * 1e-9,
+            gap: params.isi_ms * 1e-3,
+            num_pulses: params.numPulses
+        });
+        passiveMembrane.addCurrent(pulseTrain);
+        
+
+        // simulate it
+        result = model.integrate({
+            tMin: 0, 
+            tMax: params.totalDuration_ms * 1e-3, 
+            tMaxStep: Math.min(1e-4, params.C_nF / params.g_leak_uS * 1e-3) 
+        });
+        
+        v     = result.mapOrderedPairs(passiveMembrane.V);
+        iLeak = result.mapOrderedPairs(passiveMembrane.leak.current);
+        iStim = result.mapOrderedPairs(pulseTrain);
+
+        // convert to the right units
+        // each ordered pair consists of a time and another variable
+        v_mV     = v.map     (function (v) {return [v[0] / 1e-3,  v[1] / 1e-3];});
+        iLeak_nA = iLeak.map (function (i) {return [i[0] / 1e-3, -i[1] / 1e-9];});
+        iStim_nA = iStim.map (function (i) {return [i[0] / 1e-3,  i[1] / 1e-9];});
+
+        // free resources from old plots
+        while (plotHandles.length > 0) {
+            plotHandles.pop().destroy();
+        }
+
+        // plot the results
+        plotPanel = document.getElementById('MultiConductancePlots');
+        plotPanel.innerHTML = '';
+
+        // Voltage
+        plot = document.createElement('div');
+        plot.id = 'voltagePlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+           $.jqplot('voltagePlot', [v_mV], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {label:'Membrane Potential (mV)'},
+                },
+                series: [
+                    {label: 'V<sub>m</sub>', color: 'black'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#voltagePlot', voltageDataTable, 'Membrane Potential', 'Time');
+        graphJqplot.bindCursorTooltip('#voltagePlot', 'Time', 'ms', 'mV');
+
+        // Currents
+        plot = document.createElement('div');
+        plot.id = 'currentPlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+            $.jqplot('currentPlot', [iLeak_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {label:'Membrane Current (nA)'},
+                },
+                series: [
+                    {label: 'I<sub>leak</sub>', color: 'black'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#currentPlot', currentDataTable, 'Membrane Current', 'Time');
+        graphJqplot.bindCursorTooltip('#currentPlot', 'Time', 'ms', 'nA');
+
+        // Stimulus current
+        plot = document.createElement('div');
+        plot.id = 'stimPlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+            $.jqplot('stimPlot', [iStim_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {label:'Stimulation Current (nA)'},
+                },
+                series: [
+                    {label: 'I<sub>stim</sub>', color: 'black'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#stimPlot', stimDataTable, 'Stimulation Current', 'Time');
+        graphJqplot.bindCursorTooltip('#stimPlot', 'Time', 'ms', 'nA');
+    }
+
+    
+    function reset() {
+        controlsPanel.innerHTML = '';
+        controls = simcontrols.controls(controlsPanel, params, layout);
+        runSimulation();
+    }
+
+
+    function clearDataTables() {
+        voltageDataTable.innerHTML = '';
+        voltageDataTable.style.display = 'none';
+
+        currentDataTable.innerHTML = '';
+        currentDataTable.style.display = 'none';
+
+        stimDataTable.innerHTML = '';
+        stimDataTable.style.display = 'none';
+    }
+
+
+    (document.getElementById('MultiConductanceRunButton')
+        .addEventListener('click', runSimulation, false));
+    (document.getElementById('MultiConductanceResetButton')
+        .addEventListener('click', reset, false));
+    (document.getElementById('MultiConductanceClearDataButton')
+        .addEventListener('click', clearDataTables, false));
+    
+
+    // make the enter key run the simulation  
+    controlsPanel.addEventListener('keydown',  
+        function (evt, element) {
+            if (evt.keyCode === 13) { // enter was pressed 
+                controls.triggerRead();
+                runSimulation();
+                return false;
+            }
+        }, true);
+
+    reset();
+    clearDataTables();
+
+}, false);
+

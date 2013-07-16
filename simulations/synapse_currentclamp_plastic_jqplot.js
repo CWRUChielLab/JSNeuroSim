d9ea3e0d6ad3dcbd3887a607ffd1f7200ec8319b
Jeffrey Gill <jpg18@case.edu>
2013-07-16 16:27:48 -0400

    Added plastic synapse to synapse_currentclamp_plastic

--- simulations/synapse_currentclamp_plastic_jqplot.js
+++ simulations/synapse_currentclamp_plastic_jqplot.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function () {
     'use strict';
 
     var params, layout, controlsPanel, controls, dataPanel, preVoltageDataTable,
-        preCurrentDataTable, postVoltageDataTable, postCurrentDataTable,
+        preCaConcDataTable, postVoltageDataTable, postCaConcDataTable, preCurrentDataTable,
         tMax = 1000e-3, plotHandles = []; 
 
     // set up the controls for the current clamp simulation
@@ -26,29 +26,12 @@ window.addEventListener('load', function () {
             units: '\u00B5S', defaultVal: 36, minVal: 0, maxVal: 1000 }, 
         E_K_pre_mV: { label: 'Potassium Nernst potential', units: 'mV',
             defaultVal: -77, minVal: -1000, maxVal: 1000 }, 
-        pulseStart_pre_ms: { label: 'Stimulus delay', units: 'ms', 
-            defaultVal: 2, minVal: 0, maxVal: tMax / 1e-3 },
-        pulseHeight_pre_nA: { label: 'Stimulus current', units: 'nA', 
-            defaultVal: 10, minVal: -1000, maxVal: 1000 },
-        pulseWidth_pre_ms: { label: 'Pulse duration', units: 'ms', 
-            defaultVal: 4, minVal: 0, maxVal: tMax / 1e-3 },
-        isi_pre_ms: { label: 'Inter-stimulus interval', units: 'ms', 
-            defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
-        numPulses_pre: { label: 'Number of pulses', units: '', 
-            defaultVal: 2, minVal: 0, maxVal: 100 },
-
-        g_syn_uS: { label: 'Maximum conductance', units: '\u00B5S', 
-            defaultVal: 0.5, minVal: 0, maxVal: 1000 },
-        E_rev_syn_mV: { label: 'Reversal potential', units: 'mV', 
-            defaultVal: 0, minVal: -1000, maxVal: 1000 },
-        tau_r_ms: { label: 'Rise time constant', units: 'ms', 
-            defaultVal: 10, minVal: 0.1, maxVal: 1000 },
-        tau_d_ms: { label: 'Decay time constant', units: 'ms', 
-            defaultVal: 10, minVal: 0.1, maxVal: 1000 },
-        V_thresh_mV: { label: 'Threshold potential', units: 'mV', 
-            defaultVal: 2, minVal: -1000, maxVal: 1000 },
-        K_p: { label: 'Threshold width', units: 'mV', 
-            defaultVal: 5, minVal: 0.1, maxVal: 1000 },
+        g_P_pre_uS: { label: 'P-current conductance', units: '\u00B5S',
+            defaultVal: 0.05, minVal: 0, maxVal: 100},
+        E_Ca_pre_mV: { label: 'Calcium Nernst potential', units: 'mV',
+            defaultVal: 40, minVal: -1000, maxVal: 1000},
+        Ca_buff_pre_ms: { label: 'Calcium buffering time constant', units: 'ms',
+            defaultVal: 25, minVal: 0.001, maxVal: 1000},
 
         C_post_nF: { label: 'Membrane capacitance', units: 'nF',
             defaultVal: 1, minVal: 0.01, maxVal: 100 }, 
@@ -64,35 +47,54 @@ window.addEventListener('load', function () {
             units: '\u00B5S', defaultVal: 36, minVal: 0, maxVal: 1000 }, 
         E_K_post_mV: { label: 'Potassium Nernst potential', units: 'mV',
             defaultVal: -77, minVal: -1000, maxVal: 1000 }, 
-        pulseStart_post_ms: { label: 'Stimulus delay', units: 'ms', 
-            defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
-        pulseHeight_post_nA: { label: 'Stimulus current', units: 'nA', 
+        g_P_post_uS: { label: 'P-current conductance', units: '\u00B5S',
+            defaultVal: 0.05, minVal: 0, maxVal: 100},
+        E_Ca_post_mV: { label: 'Calcium Nernst potential', units: 'mV',
+            defaultVal: 40, minVal: -1000, maxVal: 1000},
+        Ca_buff_post_ms: { label: 'Calcium buffering time constant', units: 'ms',
+            defaultVal: 25, minVal: 0.001, maxVal: 1000},
+
+        g_normalized_syn_uS: { label: 'Normalized conductance', units: '\u00B5S', 
+            defaultVal: 0.2, minVal: 0, maxVal: 1000 },
+        E_rev_syn_mV: { label: 'Reversal potential', units: 'mV', 
             defaultVal: 0, minVal: -1000, maxVal: 1000 },
-        pulseWidth_post_ms: { label: 'Pulse duration', units: 'ms', 
-            defaultVal: 4, minVal: 0, maxVal: tMax / 1e-3 },
-        isi_post_ms: { label: 'Inter-stimulus interval', units: 'ms', 
+        tau_r_ms: { label: 'Rise time constant', units: 'ms', 
+            defaultVal: 10, minVal: 0.1, maxVal: 1000 },
+        tau_d_ms: { label: 'Decay time constant', units: 'ms', 
+            defaultVal: 10, minVal: 0.1, maxVal: 1000 },
+        Ca_facilitation_nM: { label: 'Calcium facilitation constant', units: 'nM',
+            defaultVal: 3, minVal: 0.001, maxVal: 1000 },
+        V_thresh_mV: { label: 'Threshold potential', units: 'mV', 
+            defaultVal: 2, minVal: -1000, maxVal: 1000 },
+        K_p: { label: 'Threshold width', units: 'mV', 
+            defaultVal: 5, minVal: 0.1, maxVal: 1000 },
+
+        pulseStart_pre_ms: { label: 'Stimulus delay', units: 'ms', 
             defaultVal: 10, minVal: 0, maxVal: tMax / 1e-3 },
-        numPulses_post: { label: 'Number of pulses', units: '', 
-            defaultVal: 2, minVal: 0, maxVal: 100 },
+        pulseHeight_pre_nA: { label: 'Stimulus current', units: 'nA', 
+            defaultVal: 10, minVal: -1000, maxVal: 1000 },
+        pulseWidth_pre_ms: { label: 'Pulse duration', units: 'ms', 
+            defaultVal: 2, minVal: 0, maxVal: tMax / 1e-3 },
+        isi_pre_ms: { label: 'Inter-stimulus interval', units: 'ms', 
+            defaultVal: 20, minVal: 0, maxVal: tMax / 1e-3 },
+        numPulses_pre: { label: 'Number of pulses', units: '', 
+            defaultVal: 6, minVal: 0, maxVal: 100 },
 
         totalDuration_ms: { label: 'Total duration', units: 'ms', 
-            defaultVal: 40, minVal: 0, maxVal: tMax / 1e-3 }
+            defaultVal: 150, minVal: 0, maxVal: tMax / 1e-3 }
     };
     layout = [
         ['Presynaptic Cell Properties', ['C_pre_nF', 'g_leak_pre_uS', 
             'E_leak_pre_mV', 'g_Na_pre_uS', 'E_Na_pre_mV', 'g_K_pre_uS', 
-            'E_K_pre_mV']],
+            'E_K_pre_mV', 'g_P_pre_uS', 'E_Ca_pre_mV', 'Ca_buff_pre_ms']],
+        ['Postsynaptic Cell Properties', ['C_post_nF', 'g_leak_post_uS', 
+            'E_leak_post_mV', 'g_Na_post_uS', 'E_Na_post_mV', 'g_K_post_uS', 
+            'E_K_post_mV', 'g_P_post_uS', 'E_Ca_post_mV', 'Ca_buff_post_ms']],
+        ['Synapse properties', [ 'g_normalized_syn_uS', 'E_rev_syn_mV', 'tau_r_ms',
+            'tau_d_ms', 'Ca_facilitation_nM', 'V_thresh_mV', 'K_p']],
         ['Presynaptic Current Clamp', ['pulseStart_pre_ms', 
             'pulseHeight_pre_nA', 'pulseWidth_pre_ms', 'isi_pre_ms', 
             'numPulses_pre']],
-        ['Synapse properties', [ 'g_syn_uS', 'E_rev_syn_mV', 'tau_r_ms',
-            'tau_d_ms', 'V_thresh_mV', 'K_p']],
-        ['Postsynaptic Cell Properties', ['C_post_nF', 'g_leak_post_uS', 
-            'E_leak_post_mV', 'g_Na_post_uS', 'E_Na_post_mV', 'g_K_post_uS', 
-            'E_K_post_mV']],
-        ['Postsynaptic Current Clamp', ['pulseStart_post_ms', 
-            'pulseHeight_post_nA', 'pulseWidth_post_ms', 'isi_post_ms', 
-            'numPulses_post']],
         ['Simulation Settings', ['totalDuration_ms']]
     ];
     controlsPanel = document.getElementById('SynapseCurrentClampPlasticControls');
@@ -105,26 +107,31 @@ window.addEventListener('load', function () {
     preVoltageDataTable.className = 'datatable';
     dataPanel.appendChild(preVoltageDataTable);
 
-    preCurrentDataTable = document.createElement('table');
-    preCurrentDataTable.className = 'datatable';
-    dataPanel.appendChild(preCurrentDataTable);
+    preCaConcDataTable = document.createElement('table');
+    preCaConcDataTable.className = 'datatable';
+    dataPanel.appendChild(preCaConcDataTable);
 
     postVoltageDataTable = document.createElement('table');
     postVoltageDataTable.className = 'datatable';
     dataPanel.appendChild(postVoltageDataTable);
 
-    postCurrentDataTable = document.createElement('table');
-    postCurrentDataTable.className = 'datatable';
-    dataPanel.appendChild(postCurrentDataTable);
+    postCaConcDataTable = document.createElement('table');
+    postCaConcDataTable.className = 'datatable';
+    dataPanel.appendChild(postCaConcDataTable);
+
+    preCurrentDataTable = document.createElement('table');
+    preCurrentDataTable.className = 'datatable';
+    dataPanel.appendChild(preCurrentDataTable);
 
     // simulate and plot an hh neuron with a pulse
     function runSimulation() {
         var model, synapse,
-            neuron_pre, pulseTrain_pre, hhKCurrent_pre, hhNaCurrent_pre,
-            neuron_post, pulseTrain_post, hhKCurrent_post, hhNaCurrent_post,
+            neuron_pre, pulseTrain_pre, hhKCurrent_pre, hhNaCurrent_pre, PCurrent_pre,
+            neuron_post, hhKCurrent_post, hhNaCurrent_post, PCurrent_post,
             prerun, result,
-            v_pre, v_pre_mV, iStim_pre, iStim_pre_nA, 
-            v_post, v_post_mV, iStim_post, iStim_post_nA,
+            v_pre, v_pre_mV, CaConc_pre, CaConc_pre_nM,
+            v_post, v_post_mV, CaConc_post, CaConc_post_nM,
+            iStim_pre, iStim_pre_nA, 
             params, plot, plotPanel, title, j; 
         
         params = controls.values;
@@ -134,7 +141,10 @@ window.addEventListener('load', function () {
         neuron_pre = electrophys.passiveMembrane(model, {
             C: params.C_pre_nF * 1e-9, 
             g_leak: params.g_leak_pre_uS * 1e-6, 
-            E_leak: params.E_leak_pre_mV * 1e-3 
+            E_leak: params.E_leak_pre_mV * 1e-3,
+            Ca_init: 0,
+            K1: 5e8, // uM C^-1
+            K2: 1e3 / params.Ca_buff_pre_ms  // s^-1
         });
 
         pulseTrain_pre = electrophys.pulseTrain({
@@ -156,22 +166,21 @@ window.addEventListener('load', function () {
             E_Na: params.E_Na_pre_mV * 1e-3
         });
         
+        PCurrent_pre = electrophys.multiConductance.PConductance(model, neuron_pre, {
+            g_P: params.g_P_pre_uS * 1e-6,
+            E_Ca: params.E_Ca_pre_mV * 1e-3
+        });
+        
         // create the postsynaptic passive membrane
         neuron_post = electrophys.passiveMembrane(model, {
             C: params.C_post_nF * 1e-9, 
             g_leak: params.g_leak_post_uS * 1e-6, 
-            E_leak: params.E_leak_post_mV * 1e-3 
+            E_leak: params.E_leak_post_mV * 1e-3,
+            Ca_init: 0,
+            K1: 5e8, // uM C^-1
+            K2: 1e3 / params.Ca_buff_post_ms  // s^-1
         });
 
-        pulseTrain_post = electrophys.pulseTrain({
-            start: 1e-3 * params.pulseStart_post_ms, 
-            width: params.pulseWidth_post_ms * 1e-3, 
-            height: params.pulseHeight_post_nA * 1e-9,
-            gap: params.isi_post_ms * 1e-3,
-            num_pulses: params.numPulses_post
-        });
-        neuron_post.addCurrent(pulseTrain_post);
-        
         hhKCurrent_post = electrophys.hhKConductance(model, neuron_post, {
             g_K: params.g_K_post_uS * 1e-6,
             E_K: params.E_K_post_mV * 1e-3
@@ -182,12 +191,16 @@ window.addEventListener('load', function () {
             E_Na: params.E_Na_post_mV * 1e-3
         });
 
+        PCurrent_post = electrophys.multiConductance.PConductance(model, neuron_post, {
+            g_P: params.g_P_post_uS * 1e-6,
+            E_Ca: params.E_Ca_post_mV * 1e-3
+        });
+        
 
         // create the synapse
-        synapse = electrophys.synapse(model, neuron_pre, neuron_post, {
-            g_bar: params.g_syn_uS * 1e-6 / (
-                1 / (1 + params.tau_r_ms / params.tau_d_ms)
-            ), 
+        synapse = electrophys.plasticSynapse(model, neuron_pre, neuron_post, {
+            g_normalized: params.g_normalized_syn_uS * 1e-6, 
+            Ca_facilitation: params.Ca_facilitation_nM * 1e-3,
             E_rev: params.E_rev_syn_mV * 1e-3,
             a_r: 1 / params.tau_r_ms * 1e3, 
             a_d: 1 / params.tau_d_ms * 1e3, 
@@ -211,17 +224,19 @@ window.addEventListener('load', function () {
             y0: prerun.y_f
         });
         
-        v_pre      = result.mapOrderedPairs(neuron_pre.V);
-        v_post     = result.mapOrderedPairs(neuron_post.V);
-        iStim_pre  = result.mapOrderedPairs(pulseTrain_pre);
-        iStim_post = result.mapOrderedPairs(pulseTrain_post);
+        v_pre       = result.mapOrderedPairs(neuron_pre.V);
+        CaConc_pre  = result.mapOrderedPairs(neuron_pre.Ca);
+        v_post      = result.mapOrderedPairs(neuron_post.V);
+        CaConc_post = result.mapOrderedPairs(neuron_post.Ca);
+        iStim_pre   = result.mapOrderedPairs(pulseTrain_pre);
 
         // convert to the right units
         // each ordered pair consists of a time and another variable
-        v_pre_mV      = v_pre.map      (function (v) {return [v[0] / 1e-3, v[1] / 1e-3];});
-        v_post_mV     = v_post.map     (function (v) {return [v[0] / 1e-3, v[1] / 1e-3];});
-        iStim_pre_nA  = iStim_pre.map  (function (i) {return [i[0] / 1e-3, i[1] / 1e-9]});
-        iStim_post_nA = iStim_post.map (function (i) {return [i[0] / 1e-3, i[1] / 1e-9]});
+        v_pre_mV       = v_pre.map       (function (v) {return [v[0] / 1e-3, v[1] / 1e-3];});
+        CaConc_pre_nM  = CaConc_pre.map  (function (c) {return [c[0] / 1e-3, c[1] / 1e-3];});
+        v_post_mV      = v_post.map      (function (v) {return [v[0] / 1e-3, v[1] / 1e-3];});
+        CaConc_post_nM = CaConc_post.map (function (c) {return [c[0] / 1e-3, c[1] / 1e-3];});
+        iStim_pre_nA   = iStim_pre.map   (function (i) {return [i[0] / 1e-3, i[1] / 1e-9]});
 
         // free resources from old plots
         while (plotHandles.length > 0) {
@@ -255,29 +270,29 @@ window.addEventListener('load', function () {
         graphJqplot.bindDataCapture('#preVoltagePlot', preVoltageDataTable, title.innerHTML, 'Time');
         graphJqplot.bindCursorTooltip('#preVoltagePlot', 'Time', 'ms', 'mV');
 
-        // Pre Current
+        // Pre Calcium
         title = document.createElement('h4');
-        title.innerHTML = 'Presynaptic Stimulation Current';
+        title.innerHTML = 'Presynaptic Intracellular Calcium Concentration';
         title.className = 'simplotheading';
         plotPanel.appendChild(title);
         plot = document.createElement('div');
-        plot.id = 'preCurrentPlot';
+        plot.id = 'preCaConcPlot';
         plot.style.width = '425px';
         plot.style.height = '200px';
         plotPanel.appendChild(plot);
         plotHandles.push(
-           $.jqplot('preCurrentPlot', [iStim_pre_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+            $.jqplot('preCaConcPlot', [CaConc_pre_nM], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
                 axes: {
                     xaxis: {label:'Time (ms)'},
-                    yaxis: {label:'Current (nA)'},
+                    yaxis: {label:'Calcium Concentration (nM)'},
                 },
                 series: [
-                    {label: 'I<sub>stim</sub>', color: 'black'},
+                    {label: '[Ca]', color: 'black'},
                 ],
         })));
-        graphJqplot.bindDataCapture('#preCurrentPlot', preCurrentDataTable, title.innerHTML, 'Time');
-        graphJqplot.bindCursorTooltip('#preCurrentPlot', 'Time', 'ms', 'nA');
-
+        graphJqplot.bindDataCapture('#preCaConcPlot', preCaConcDataTable, 'Presynaptic Intracellular Ca Concentration', 'Time');
+        graphJqplot.bindCursorTooltip('#preCaConcPlot', 'Time', 'ms', 'nM');
+        
         // Post Voltage
         title = document.createElement('h4');
         title.innerHTML = 'Postsynaptic Membrane Potential';
@@ -301,18 +316,41 @@ window.addEventListener('load', function () {
         graphJqplot.bindDataCapture('#postVoltagePlot', postVoltageDataTable, title.innerHTML, 'Time');
         graphJqplot.bindCursorTooltip('#postVoltagePlot', 'Time', 'ms', 'mV');
 
-        // Post Current
+        // Post Calcium
         title = document.createElement('h4');
-        title.innerHTML = 'Postsynaptic Stimulation Current';
+        title.innerHTML = 'Postsynaptic Intracellular Calcium Concentration';
         title.className = 'simplotheading';
         plotPanel.appendChild(title);
         plot = document.createElement('div');
-        plot.id = 'postCurrentPlot';
+        plot.id = 'postCaConcPlot';
         plot.style.width = '425px';
         plot.style.height = '200px';
         plotPanel.appendChild(plot);
         plotHandles.push(
-           $.jqplot('postCurrentPlot', [iStim_post_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+            $.jqplot('postCaConcPlot', [CaConc_post_nM], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
+                axes: {
+                    xaxis: {label:'Time (ms)'},
+                    yaxis: {label:'Calcium Concentration (nM)'},
+                },
+                series: [
+                    {label: '[Ca]', color: 'black'},
+                ],
+        })));
+        graphJqplot.bindDataCapture('#postCaConcPlot', postCaConcDataTable, 'Postsynaptic Intracellular Ca Concentration', 'Time');
+        graphJqplot.bindCursorTooltip('#postCaConcPlot', 'Time', 'ms', 'nM');
+        
+        // Pre Current
+        title = document.createElement('h4');
+        title.innerHTML = 'Presynaptic Stimulation Current';
+        title.className = 'simplotheading';
+        plotPanel.appendChild(title);
+        plot = document.createElement('div');
+        plot.id = 'preCurrentPlot';
+        plot.style.width = '425px';
+        plot.style.height = '200px';
+        plotPanel.appendChild(plot);
+        plotHandles.push(
+           $.jqplot('preCurrentPlot', [iStim_pre_nA], jQuery.extend(true, {}, graphJqplot.defaultOptions(params), {
                 axes: {
                     xaxis: {label:'Time (ms)'},
                     yaxis: {label:'Current (nA)'},
@@ -321,8 +359,8 @@ window.addEventListener('load', function () {
                     {label: 'I<sub>stim</sub>', color: 'black'},
                 ],
         })));
-        graphJqplot.bindDataCapture('#postCurrentPlot', postCurrentDataTable, title.innerHTML, 'Time');
-        graphJqplot.bindCursorTooltip('#postCurrentPlot', 'Time', 'ms', 'nA');
+        graphJqplot.bindDataCapture('#preCurrentPlot', preCurrentDataTable, title.innerHTML, 'Time');
+        graphJqplot.bindCursorTooltip('#preCurrentPlot', 'Time', 'ms', 'nA');
     }
 
 
@@ -337,14 +375,17 @@ window.addEventListener('load', function () {
         preVoltageDataTable.innerHTML = '';
         preVoltageDataTable.style.display = 'none';
 
-        preCurrentDataTable.innerHTML = '';
-        preCurrentDataTable.style.display = 'none';
+        preCaConcDataTable.innerHTML = '';
+        preCaConcDataTable.style.display = 'none';
 
         postVoltageDataTable.innerHTML = '';
         postVoltageDataTable.style.display = 'none';
 
-        postCurrentDataTable.innerHTML = '';
-        postCurrentDataTable.style.display = 'none';
+        postCaConcDataTable.innerHTML = '';
+        postCaConcDataTable.style.display = 'none';
+
+        preCurrentDataTable.innerHTML = '';
+        preCurrentDataTable.style.display = 'none';
     }
 
 

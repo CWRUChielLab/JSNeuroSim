46752c02e41d1403b680974ef1521cccd9724600
Jeffrey Gill <jpg18@case.edu>
2014-11-06 22:34:55 -0500

    Added (hidden) "Download SVG" button to reflex sim

--- simulations/reflex_jqplot.js
+++ simulations/reflex_jqplot.js
@@ -726,6 +726,22 @@ window.addEventListener('load', function () {
 			}, 100);
 
 	}
+
+    function downloadSVG() {
+        var svgString, a, blob;
+
+        svgString = paper.toSVG();
+        a = document.createElement('a');
+        animationPanel.appendChild(a);
+        a.download = 'reflex-animation.svg';
+        a.type = 'image/svg+xml';
+        blob = new Blob([svgString], {'type': 'image/svg+xml'});
+        a.href = (window.URL || webkitURL).createObjectURL(blob);
+        a.click();
+    }
+
+    // Comment out this line to show the "Download SVG" button
+    document.getElementById('DownloadSVGButton').style.display = 'none';
 	
 
 	(document.getElementById('ReflexRunButton')
@@ -738,6 +754,8 @@ window.addEventListener('load', function () {
 		.addEventListener('click', reflexAnimation, false));
 	(document.getElementById('ResetAnimationButton')
 		.addEventListener('click', resetAnimation, false));
+	(document.getElementById('DownloadSVGButton')
+		.addEventListener('click', downloadSVG, false));
 	
 
 	// make the enter key run the simulation  
